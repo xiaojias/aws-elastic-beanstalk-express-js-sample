ccc94025db7f09a11a3a05f4e237702619ce6ec3
Xiao Jia shan <649788479@qq.com>
2021-05-19 17:47:41 +0800

    Update app.js
    Updated again

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 const port = 8080;
 
-app.get('/', (req, res) => res.send('Hello World! Updated on May 19, 2021'));
+app.get('/', (req, res) => res.send('Hello World! \n Updated again by Richard on May 19, 2021'));
 
 app.listen(port);
 console.log(`App running on http://localhost:${port}`);
